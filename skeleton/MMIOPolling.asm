992c6d065e7bf5e51e29050c33426210a1200b9c
Andi <anke00005@stud.uni-saarland.de>
2023-07-06 18:20:42 +0200

    Polling done?

--- skeleton/MMIOPolling.asm
+++ skeleton/MMIOPolling.asm
@@ -5,5 +5,16 @@
 
 .ktext
 start:
+	la $t1, 0xffff0000 #store the address of the control port of the keyboard
+	la $t2 , 0xffff0004 # store the address of the data port of the keyboard
+	la $t5 0xffff000c #only store the address of the data port of the display since we dont
+			  # need to check if the display is ready because we dont have any user code
+loop:
+	lw $t3, ($t1) #
+	beq $t3, $zero, loop
+	lw $t4, ($t2)
+	sw $t4, ($t5)
+	j loop
 	# TODO Implement input/output with polling
 	b start
+	
